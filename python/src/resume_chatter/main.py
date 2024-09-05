f40d6edd568bf932d717de48e4f060abb846a76b
Brian Fallik <bfallik@gmail.com>
2024-09-05 16:49:30 -0400

    thanks Ruff

--- python/src/resume_chatter/main.py
+++ python/src/resume_chatter/main.py
@@ -1,3 +1,5 @@
+import logging
+
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import PyPDFLoader
@@ -6,18 +8,18 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-import logging
-
 logger = logging.getLogger(__name__)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     model = "gpt-4o-mini"
     logger.warning(f"using model: {model}")
 
     llm = ChatOpenAI(model=model)
 
-    loader = PyPDFLoader("/home/bfallik/Documents/JobSearches/bfallik-resume/bfallik-resume.pdf")
+    loader = PyPDFLoader(
+        "/home/bfallik/Documents/JobSearches/bfallik-resume/bfallik-resume.pdf"
+    )
     docs = loader.load()
 
     text_splitter = RecursiveCharacterTextSplitter()
@@ -37,5 +39,7 @@ if __name__=="__main__":
     retriever = vector.as_retriever()
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-    res = retrieval_chain.invoke({"input": "What was Brian's second most recent job and when did he work there?"})
+    res = retrieval_chain.invoke(
+        {"input": "What was Brian's second most recent job and when did he work there?"}
+    )
     print(res)
