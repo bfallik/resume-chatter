04eb42202555a5379523bc8228fa0dc339202b3e
Brian Fallik <bfallik@gmail.com>
2024-09-05 16:35:29 -0400

    implement POC document retrieval chain!
    Based on the Langchain tutorial: https://python.langchain.com/v0.1/docs/get_started/quickstart/
    
    This produces the correct response to the hard-coded request in the source code:
        Brian's second most recent job was as VP of Engineering at Real
        Chemistry, where he worked from October 2021 to January 2023.

--- python/src/resume_chatter/main.py
+++ python/src/resume_chatter/main.py
@@ -1,4 +1,10 @@
-from langchain_openai import ChatOpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 import logging
 
@@ -8,6 +14,28 @@ logger = logging.getLogger(__name__)
 if __name__=="__main__":
     model = "gpt-4o-mini"
     logger.warning(f"using model: {model}")
+
     llm = ChatOpenAI(model=model)
-    res = llm.invoke("how can langsmith help with testing?")
+
+    loader = PyPDFLoader("/home/bfallik/Documents/JobSearches/bfallik-resume/bfallik-resume.pdf")
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(docs)
+    embeddings = OpenAIEmbeddings()
+    vector = FAISS.from_documents(documents, embeddings)
+
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+    <context>
+    {context}
+    </context>
+
+    Question: {input}""")
+
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = vector.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+    res = retrieval_chain.invoke({"input": "What was Brian's second most recent job and when did he work there?"})
     print(res)
