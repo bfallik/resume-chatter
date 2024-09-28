61c5e0acdfbcc33fbbe7b0c7980aa06ef8acd2d6
Brian Fallik <bfallik@gmail.com>
2024-09-28 11:45:48 -0400

    refactor: create a proper server struct

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	rc "github.com/bfallik/resume-chatter"
 )
 
 func main() {
+	ctx := context.Background()
 	const a = ":8080"
-	log.Fatalln(rc.Serve(a))
+
+	svr, err := rc.NewServer(ctx)
+	if err != nil {
+		log.Fatalln("error creating server: ", err)
+	}
+
+	log.Fatalln(svr.Serve(a))
 }
--- root.go
+++ root.go
@@ -19,6 +19,7 @@ import (
 	"github.com/tmc/langchaingo/chains"
 	"github.com/tmc/langchaingo/documentloaders"
 	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/schema"
 	"github.com/tmc/langchaingo/textsplitter"
 
 	"github.com/go-chi/chi/middleware"
@@ -53,22 +54,6 @@ func (h *History) UpdateWaiting(newBubble string) {
 	h.data[ln-1].Bubble = newBubble
 }
 
-var chatHistory History = History{
-	data: []model.ChatMessage{{
-		IsStart:   true,
-		Header:    "Obi-Wan Kenobi",
-		IsWaiting: false,
-		Bubble:    "You were the Chosen One!",
-	},
-		{
-			IsStart:   false,
-			Header:    "Anakin",
-			IsWaiting: false,
-			Bubble:    "I loved you.",
-		},
-	},
-}
-
 type LLMAlert struct {
 	llmErr error
 	mu     sync.RWMutex
@@ -86,33 +71,164 @@ func (a *LLMAlert) SetErr(err error) {
 	a.llmErr = err
 }
 
-var alert LLMAlert = LLMAlert{}
+type Server struct {
+	Start       time.Time
+	LLM         *openai.LLM
+	Docs        []schema.Document
+	ChatHistory History
+	Alert       LLMAlert
+}
 
-func Serve(address string) error {
+func NewServer(ctx context.Context) (*Server, error) {
 	start := time.Now()
 	log.Printf("started %v", start.Format(time.RFC1123))
 
-	ctx := context.Background()
-
 	llm, err := openai.New()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	buf := new(bytes.Buffer)
 	cmd := exec.Command("pdftotext", "/home/bfallik/Documents/JobSearches/bfallik-resume/bfallik-resume.pdf", "-")
 	cmd.Stdout = buf
 	if err := cmd.Run(); err != nil {
-		return err
+		return nil, err
 	}
 
 	loader := documentloaders.NewText(buf)
 	docs, err := loader.LoadAndSplit(ctx, textsplitter.NewRecursiveCharacter())
 	if err != nil {
-		return err
+		return nil, err
 	}
 
+	return &Server{
+		Start: start,
+		LLM:   llm,
+		Docs:  docs,
+		ChatHistory: History{
+			data: []model.ChatMessage{
+				{
+					IsStart:   true,
+					Header:    "Obi-Wan Kenobi",
+					IsWaiting: false,
+					Bubble:    "You were the Chosen One!",
+				},
+				{
+					IsStart:   false,
+					Header:    "Anakin",
+					IsWaiting: false,
+					Bubble:    "I loved you.",
+				},
+			},
+		},
+	}, nil
+}
+
+func (s *Server) RootHandler(w http.ResponseWriter, r *http.Request) {
+	idx := pages.Index(s.ChatHistory.GetChat(), s.Start)
+	err := idx.Render(r.Context(), w)
+	if err != nil {
+		log.Printf("err rendering html template: %+v\n", err)
+		http.Error(w, "error rendering HTML template", http.StatusInternalServerError)
+	}
+}
+
+func (s *Server) AskHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Printf("err parsing form: %+v\n", err)
+		http.Error(w, "error parsing form", http.StatusInternalServerError)
+		return
+	}
+
+	content, ok := r.Form["content"]
+	if !ok {
+		log.Printf("missing form value: content\n")
+		http.Error(w, "missing form value: content", http.StatusInternalServerError)
+		return
+	}
+
+	question := content[0] // BF TODO: handle this
+	log.Println("question: ", question)
+	ln := len(s.ChatHistory.GetChat())
+	newMsgs := []model.ChatMessage{
+		{
+			IsStart:   true,
+			Header:    "Obi-Wan Kenobi",
+			IsWaiting: false,
+			Bubble:    question,
+		},
+		{
+			IsStart:             false,
+			Header:              "Anakin",
+			IsWaiting:           true,
+			WaitingMessageIndex: ln + 1,
+			Bubble:              "",
+		},
+	}
+	s.ChatHistory.Append(newMsgs...)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+
+		// TODO - find similar docs
+
+		stuffQAChain := chains.LoadStuffQA(s.LLM)
+		answer, err := chains.Call(context.Background(), stuffQAChain, map[string]any{
+			"input_documents": s.Docs,
+			"question":        question,
+		})
+		if err != nil {
+			log.Printf("error calling chain: %+v\n", err)
+			s.Alert.SetErr(errors.New("error calling LLM"))
+			return
+		}
+
+		s.ChatHistory.UpdateWaiting(fmt.Sprintf("%v", answer["text"]))
+		log.Println("answer: ", answer)
+	}()
+
+	if err := components.ChatHistoryElements(newMsgs...).Render(r.Context(), w); err != nil {
+		log.Printf("err rendering html template: %+v\n", err)
+		http.Error(w, "error rendering HTML template", http.StatusInternalServerError)
+	}
+}
+
+func (s *Server) MessageHandler(w http.ResponseWriter, r *http.Request) {
+	idxStr := r.URL.Query().Get("index")
+	n, err := strconv.Atoi(idxStr)
+	if err != nil {
+		log.Printf("err converting index '%v': %+v\n", idxStr, err)
+		http.Error(w, "error converting index", http.StatusBadRequest)
+		return
+	}
+
+	h := s.ChatHistory.GetChat()
+	ln := len(h)
+	if n >= ln {
+		log.Printf("err converting index '%v': %+v\n", idxStr, err)
+		http.Error(w, "error converting index", http.StatusNotFound)
+		return
+	}
+
+	if err := components.ChatHistoryElements(h[n]).Render(r.Context(), w); err != nil {
+		log.Printf("err rendering html template: %+v\n", err)
+		http.Error(w, "error rendering HTML template", http.StatusInternalServerError)
+		return
+	}
+
+	alertErr := s.Alert.GetErr()
+	if alertErr != nil {
+		if err := components.Alert(model.Alert{MsgText: alertErr.Error()}).Render(r.Context(), w); err != nil {
+			log.Printf("err rendering html template: %+v\n", err)
+			http.Error(w, "error rendering HTML template", http.StatusInternalServerError)
+		}
+	}
+}
+
+func (s *Server) Serve(address string) error {
 	r := chi.NewRouter()
+
+	// middleware
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
@@ -120,109 +236,13 @@ func Serve(address string) error {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 
+	// routes
+
 	r.Handle("/static/*", http.FileServer(http.FS(staticFS)))
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		idx := pages.Index(chatHistory.GetChat(), start)
-		err := idx.Render(r.Context(), w)
-		if err != nil {
-			log.Printf("err rendering html template: %+v\n", err)
-			http.Error(w, "error rendering HTML template", http.StatusInternalServerError)
-		}
-	})
-
-	r.Post("/ask", func(w http.ResponseWriter, r *http.Request) {
-		if err := r.ParseForm(); err != nil {
-			log.Printf("err parsing form: %+v\n", err)
-			http.Error(w, "error parsing form", http.StatusInternalServerError)
-			return
-		}
-
-		content, ok := r.Form["content"]
-		if !ok {
-			log.Printf("missing form value: content\n")
-			http.Error(w, "missing form value: content", http.StatusInternalServerError)
-			return
-		}
-
-		question := content[0] // BF TODO: handle this
-		log.Println("question: ", question)
-		ln := len(chatHistory.GetChat())
-		newMsgs := []model.ChatMessage{
-			{
-				IsStart:   true,
-				Header:    "Obi-Wan Kenobi",
-				IsWaiting: false,
-				Bubble:    question,
-			},
-			{
-				IsStart:             false,
-				Header:              "Anakin",
-				IsWaiting:           true,
-				WaitingMessageIndex: ln + 1,
-				Bubble:              "",
-			},
-		}
-		chatHistory.Append(newMsgs...)
-
-		go func() {
-			time.Sleep(2 * time.Second)
-
-			// TODO - find similar docs
-
-			stuffQAChain := chains.LoadStuffQA(llm)
-			answer, err := chains.Call(context.Background(), stuffQAChain, map[string]any{
-				"input_documents": docs,
-				"question":        question,
-			})
-			if err != nil {
-				log.Printf("error calling chain: %+v\n", err)
-				alert.SetErr(errors.New("error calling LLM"))
-				return
-			}
-
-			chatHistory.UpdateWaiting(fmt.Sprintf("%v", answer["text"]))
-			log.Println("answer: ", answer)
-		}()
-
-		if err := components.ChatHistoryElements(newMsgs...).Render(r.Context(), w); err != nil {
-			log.Printf("err rendering html template: %+v\n", err)
-			http.Error(w, "error rendering HTML template", http.StatusInternalServerError)
-		}
-	})
-
-	r.Get("/message", func(w http.ResponseWriter, r *http.Request) {
-		idxStr := r.URL.Query().Get("index")
-		n, err := strconv.Atoi(idxStr)
-		if err != nil {
-			log.Printf("err converting index '%v': %+v\n", idxStr, err)
-			http.Error(w, "error converting index", http.StatusBadRequest)
-			return
-		}
-
-		h := chatHistory.GetChat()
-		ln := len(h)
-		if n >= ln {
-			log.Printf("err converting index '%v': %+v\n", idxStr, err)
-			http.Error(w, "error converting index", http.StatusNotFound)
-			return
-		}
-
-		if err := components.ChatHistoryElements(h[n]).Render(r.Context(), w); err != nil {
-			log.Printf("err rendering html template: %+v\n", err)
-			http.Error(w, "error rendering HTML template", http.StatusInternalServerError)
-			return
-		}
-
-		alertErr := alert.GetErr()
-		if alertErr != nil {
-			if err := components.Alert(model.Alert{MsgText: alertErr.Error()}).Render(r.Context(), w); err != nil {
-				log.Printf("err rendering html template: %+v\n", err)
-				http.Error(w, "error rendering HTML template", http.StatusInternalServerError)
-			}
-		}
-
-	})
+	r.Get("/", s.RootHandler)
+	r.Post("/ask", s.AskHandler)
+	r.Get("/message", s.MessageHandler)
 
 	log.Println("webserver listening on", address)
 	return http.ListenAndServe(address, r)
